3068cd51631e40286666b5b91a277dab01237615
13952522076 <xuma@my.unt.edu>
2020-06-24 16:04:13 -0500

    update

--- tools/test_image2.py
+++ tools/test_image2.py
@@ -21,6 +21,7 @@ import matplotlib.cm as cm
 
 def vis_seg(data, result, img_norm_cfg, data_id, colors, score_thr, save_dir):
     print(data)
+    print("img_metas: {}".format(data['img_meta'][0]))
     img_tensor = data['img'][0]
     # img_metas = data['img_meta'][0].data[0]
     imgs = tensor2imgs(img_tensor, **img_norm_cfg)
